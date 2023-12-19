be9aed3194fc5e9638d18df898be56d9bfd218fa
arkham <arkham@gmail.com>
2023-12-19 19:39:39 +0900

    Initial Commit

---
+++ arkhamswift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  arkhamswift
+//
+//  Created by 鈴木通之 on 2023/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ arkhamswift/arkhamswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  arkhamswiftApp.swift
+//  arkhamswift
+//
+//  Created by 鈴木通之 on 2023/12/19.
+//
+
+import SwiftUI
+
+@main
+struct arkhamswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
